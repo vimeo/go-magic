49383ad1c183a9037dae90638205fef1c6ebd7eb
Henry Donnay <hdonnay@gmail.com>
2018-01-26 12:43:03 -0500

    gofmt pass

--- magic/magic.go
+++ magic/magic.go
@@ -7,127 +7,127 @@ package magic
 */
 import "C"
 import (
-    "os"
-    "path"
-    "unsafe"
+	"os"
+	"path"
+	"unsafe"
 )
 
 const (
-    MAGIC_NONE              = C.MAGIC_NONE
-    MAGIC_DEBUG             = C.MAGIC_DEBUG
-    MAGIC_SYMLINK           = C.MAGIC_SYMLINK
-    MAGIC_COMPRESS          = C.MAGIC_COMPRESS
-    MAGIC_DEVICES           = C.MAGIC_DEVICES
-    MAGIC_MIME_TYPE         = C.MAGIC_MIME_TYPE
-    MAGIC_CONTINUE          = C.MAGIC_CONTINUE
-    MAGIC_CHECK             = C.MAGIC_CHECK
-    MAGIC_PRESERVE_ATIME    = C.MAGIC_PRESERVE_ATIME
-    MAGIC_RAW               = C.MAGIC_RAW
-    MAGIC_ERROR             = C.MAGIC_ERROR
-    MAGIC_MIME_ENCODING     = C.MAGIC_MIME_ENCODING
-    MAGIC_MIME              = C.MAGIC_MIME
-    MAGIC_APPLE             = C.MAGIC_APPLE
-    MAGIC_NO_CHECK_COMPRESS = C.MAGIC_NO_CHECK_COMPRESS
-    MAGIC_NO_CHECK_TAR      = C.MAGIC_NO_CHECK_TAR
-    MAGIC_NO_CHECK_SOFT     = C.MAGIC_NO_CHECK_SOFT
-    MAGIC_NO_CHECK_APPTYPE  = C.MAGIC_NO_CHECK_APPTYPE
-    MAGIC_NO_CHECK_ELF      = C.MAGIC_NO_CHECK_ELF
-    MAGIC_NO_CHECK_TEXT     = C.MAGIC_NO_CHECK_TEXT
-    MAGIC_NO_CHECK_CDF      = C.MAGIC_NO_CHECK_CDF
-    MAGIC_NO_CHECK_TOKENS   = C.MAGIC_NO_CHECK_TOKENS
-    MAGIC_NO_CHECK_ENCODING = C.MAGIC_NO_CHECK_ENCODING
-    MAGIC_NO_CHECK_ASCII    = C.MAGIC_NO_CHECK_ASCII
-    MAGIC_NO_CHECK_FORTRAN  = C.MAGIC_NO_CHECK_FORTRAN
-    MAGIC_NO_CHECK_TROFF    = C.MAGIC_NO_CHECK_TROFF
+	MAGIC_NONE              = C.MAGIC_NONE
+	MAGIC_DEBUG             = C.MAGIC_DEBUG
+	MAGIC_SYMLINK           = C.MAGIC_SYMLINK
+	MAGIC_COMPRESS          = C.MAGIC_COMPRESS
+	MAGIC_DEVICES           = C.MAGIC_DEVICES
+	MAGIC_MIME_TYPE         = C.MAGIC_MIME_TYPE
+	MAGIC_CONTINUE          = C.MAGIC_CONTINUE
+	MAGIC_CHECK             = C.MAGIC_CHECK
+	MAGIC_PRESERVE_ATIME    = C.MAGIC_PRESERVE_ATIME
+	MAGIC_RAW               = C.MAGIC_RAW
+	MAGIC_ERROR             = C.MAGIC_ERROR
+	MAGIC_MIME_ENCODING     = C.MAGIC_MIME_ENCODING
+	MAGIC_MIME              = C.MAGIC_MIME
+	MAGIC_APPLE             = C.MAGIC_APPLE
+	MAGIC_NO_CHECK_COMPRESS = C.MAGIC_NO_CHECK_COMPRESS
+	MAGIC_NO_CHECK_TAR      = C.MAGIC_NO_CHECK_TAR
+	MAGIC_NO_CHECK_SOFT     = C.MAGIC_NO_CHECK_SOFT
+	MAGIC_NO_CHECK_APPTYPE  = C.MAGIC_NO_CHECK_APPTYPE
+	MAGIC_NO_CHECK_ELF      = C.MAGIC_NO_CHECK_ELF
+	MAGIC_NO_CHECK_TEXT     = C.MAGIC_NO_CHECK_TEXT
+	MAGIC_NO_CHECK_CDF      = C.MAGIC_NO_CHECK_CDF
+	MAGIC_NO_CHECK_TOKENS   = C.MAGIC_NO_CHECK_TOKENS
+	MAGIC_NO_CHECK_ENCODING = C.MAGIC_NO_CHECK_ENCODING
+	MAGIC_NO_CHECK_ASCII    = C.MAGIC_NO_CHECK_ASCII
+	MAGIC_NO_CHECK_FORTRAN  = C.MAGIC_NO_CHECK_FORTRAN
+	MAGIC_NO_CHECK_TROFF    = C.MAGIC_NO_CHECK_TROFF
 )
 const (
-    MAGIC_NO_CHECK_BUILTIN  = MAGIC_NO_CHECK_COMPRESS |
-                              MAGIC_NO_CHECK_TAR      |
-                              MAGIC_NO_CHECK_APPTYPE  |
-                              MAGIC_NO_CHECK_ELF      |
-                              MAGIC_NO_CHECK_TEXT     |
-                              MAGIC_NO_CHECK_CDF      |
-                              MAGIC_NO_CHECK_TOKENS   |
-                              MAGIC_NO_CHECK_ENCODING
+	MAGIC_NO_CHECK_BUILTIN = MAGIC_NO_CHECK_COMPRESS |
+		MAGIC_NO_CHECK_TAR |
+		MAGIC_NO_CHECK_APPTYPE |
+		MAGIC_NO_CHECK_ELF |
+		MAGIC_NO_CHECK_TEXT |
+		MAGIC_NO_CHECK_CDF |
+		MAGIC_NO_CHECK_TOKENS |
+		MAGIC_NO_CHECK_ENCODING
 )
 
 type Magic C.magic_t
 
-var system_mgc_locations = []string {
-    "/usr/share/misc/magic.mgc",
-    "/usr/share/file/magic.mgc",
-    "/usr/share/magic/magic.mgc",
+var system_mgc_locations = []string{
+	"/usr/share/misc/magic.mgc",
+	"/usr/share/file/magic.mgc",
+	"/usr/share/magic/magic.mgc",
 }
 
 /* Find the real magic file location */
 func GetDefaultDir() string {
-    var f string
+	var f string
 
-    found_mgc := false
-    for _, f = range system_mgc_locations {
-        fi, err := os.Lstat(f)
-        if err == nil && fi.Mode() & os.ModeSymlink != os.ModeSymlink {
-            found_mgc = true
-            break
-        }
-    }
-    if found_mgc {
-        return path.Dir(f)
-    } else {
-        return ""
-    }
+	found_mgc := false
+	for _, f = range system_mgc_locations {
+		fi, err := os.Lstat(f)
+		if err == nil && fi.Mode()&os.ModeSymlink != os.ModeSymlink {
+			found_mgc = true
+			break
+		}
+	}
+	if found_mgc {
+		return path.Dir(f)
+	} else {
+		return ""
+	}
 }
 
 func Open(flags int) Magic {
-    cookie := (Magic)(C.magic_open(C.int(flags)))
-    return cookie;
+	cookie := (Magic)(C.magic_open(C.int(flags)))
+	return cookie
 }
 
 func Close(cookie Magic) {
-    C.magic_close((C.magic_t)(cookie))
+	C.magic_close((C.magic_t)(cookie))
 }
 
 func Error(cookie Magic) string {
-    s := (C.magic_error((C.magic_t)(cookie)))
-    return C.GoString(s)
+	s := (C.magic_error((C.magic_t)(cookie)))
+	return C.GoString(s)
 }
 
 func Errno(cookie Magic) int {
-    return (int)(C.magic_errno((C.magic_t)(cookie)))
+	return (int)(C.magic_errno((C.magic_t)(cookie)))
 }
 
 func File(cookie Magic, filename string) string {
-    cfilename := C.CString(filename)
-    defer C.free(unsafe.Pointer(cfilename))
-    return C.GoString(C.magic_file(cookie, cfilename))
+	cfilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfilename))
+	return C.GoString(C.magic_file(cookie, cfilename))
 }
 
 func Buffer(cookie Magic, b []byte) string {
-    length := C.size_t(len(b))
-    return C.GoString(C.magic_buffer(cookie, unsafe.Pointer(&b[0]), length))
+	length := C.size_t(len(b))
+	return C.GoString(C.magic_buffer(cookie, unsafe.Pointer(&b[0]), length))
 }
 
 func SetFlags(cookie Magic, flags int) int {
-    return (int)(C.magic_setflags(cookie, C.int(flags)))
+	return (int)(C.magic_setflags(cookie, C.int(flags)))
 }
 
 func Check(cookie Magic, filename string) int {
-    cfilename := C.CString(filename)
-    defer C.free(unsafe.Pointer(cfilename))
-    return (int)(C.magic_check(cookie, cfilename))
+	cfilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfilename))
+	return (int)(C.magic_check(cookie, cfilename))
 }
 
 func Compile(cookie Magic, filename string) int {
-    cfilename := C.CString(filename)
-    defer C.free(unsafe.Pointer(cfilename))
-    return (int)(C.magic_compile(cookie, cfilename))
+	cfilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfilename))
+	return (int)(C.magic_compile(cookie, cfilename))
 }
 
 func Load(cookie Magic, filename string) int {
-    if filename == "" {
-        return (int)(C.magic_load(cookie, nil))
-    }
-    cfilename := C.CString(filename)
-    defer C.free(unsafe.Pointer(cfilename))
-    return (int)(C.magic_load(cookie, cfilename))
+	if filename == "" {
+		return (int)(C.magic_load(cookie, nil))
+	}
+	cfilename := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfilename))
+	return (int)(C.magic_load(cookie, cfilename))
 }
--- magic/mime.go
+++ magic/mime.go
@@ -1,130 +1,130 @@
 package magic
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "path/filepath"
-    "strings"
-    "sync"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
 var magicFiles map[string]bool
 var mutex sync.Mutex
 
 func init() {
-    magicFiles = make(map[string]bool)
+	magicFiles = make(map[string]bool)
 }
 
 func compileToMgc(file string) {
-    cookie := Open(MAGIC_NONE)
-    defer Close(cookie)
-    Compile(cookie, file)
+	cookie := Open(MAGIC_NONE)
+	defer Close(cookie)
+	Compile(cookie, file)
 }
 
 func compileMagicFiles(dir string, files []string) {
-    // for some reason libmagic puts compiled files in the current working dir
-    // instead of the dir of the source file, so switch to the source dir,
-    // compile, then switch back
-    pwd, err := os.Getwd()
-    if err != nil {
-        return
-    }
-    err = os.Chdir(dir)
-    if err != nil {
-        return
-    }
-    defer os.Chdir(pwd)
+	// for some reason libmagic puts compiled files in the current working dir
+	// instead of the dir of the source file, so switch to the source dir,
+	// compile, then switch back
+	pwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	err = os.Chdir(dir)
+	if err != nil {
+		return
+	}
+	defer os.Chdir(pwd)
 
-    for _, f := range files {
-        compileToMgc(f)
-    }
+	for _, f := range files {
+		compileToMgc(f)
+	}
 }
 
 /* Add a directory for libmagic to search for .mgc databases. */
 func AddMagicDir(dir string) error {
-    var err error
+	var err error
 
-    dir, err = filepath.Abs(dir)
-    if err != nil {
-        return err
-    }
-    fi, err := os.Stat(dir)
-    if err != nil {
-        return err
-    }
-    if fi.IsDir() == false {
-        return fmt.Errorf("Not a directory: %s", dir)
-    }
+	dir, err = filepath.Abs(dir)
+	if err != nil {
+		return err
+	}
+	fi, err := os.Stat(dir)
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() == false {
+		return fmt.Errorf("Not a directory: %s", dir)
+	}
 
-    // get list of .magic files that need to be compiled to .mgc
-    var srcFiles []string
-    files, err := ioutil.ReadDir(dir)
-    if err != nil {
-        return err
-    }
-    for _, fi = range files {
-        if filepath.Ext(fi.Name()) == ".magic" {
-            mgcSrc := filepath.Join(dir, fi.Name())
-            _, err := os.Stat(mgcSrc + ".mgc")
-            if err != nil {
-                srcFiles = append(srcFiles, mgcSrc)
-            }
-        }
-    }
-    // compile .magic files
-    if len(srcFiles) > 0 {
-        compileMagicFiles(dir, srcFiles)
-    }
+	// get list of .magic files that need to be compiled to .mgc
+	var srcFiles []string
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, fi = range files {
+		if filepath.Ext(fi.Name()) == ".magic" {
+			mgcSrc := filepath.Join(dir, fi.Name())
+			_, err := os.Stat(mgcSrc + ".mgc")
+			if err != nil {
+				srcFiles = append(srcFiles, mgcSrc)
+			}
+		}
+	}
+	// compile .magic files
+	if len(srcFiles) > 0 {
+		compileMagicFiles(dir, srcFiles)
+	}
 
-    files, err = ioutil.ReadDir(dir)
-    if err != nil {
-        return err
-    }
-    mutex.Lock()
-    for _, fi = range files {
-        if filepath.Ext(fi.Name()) == ".mgc" {
-            mgcFile := filepath.Join(dir, fi.Name())
-            magicFiles[mgcFile] = true
-        }
-    }
-    mutex.Unlock()
+	files, err = ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	mutex.Lock()
+	for _, fi = range files {
+		if filepath.Ext(fi.Name()) == ".mgc" {
+			mgcFile := filepath.Join(dir, fi.Name())
+			magicFiles[mgcFile] = true
+		}
+	}
+	mutex.Unlock()
 
-    return nil
+	return nil
 }
 
 /* Get mimetype from a file. */
 func MimeFromFile(path string) string {
-    cookie := Open(MAGIC_ERROR | MAGIC_MIME_TYPE)
-    defer Close(cookie)
-    mutex.Lock()
-    var mf []string
-    for f := range magicFiles {
-        mf = append(mf, f)
-    }
-    mutex.Unlock()
-    ret := Load(cookie, strings.Join(mf, ":"))
-    if ret != 0 {
-        return "application/octet-stream"
-    }
-    r := File(cookie, path)
-    return r
+	cookie := Open(MAGIC_ERROR | MAGIC_MIME_TYPE)
+	defer Close(cookie)
+	mutex.Lock()
+	var mf []string
+	for f := range magicFiles {
+		mf = append(mf, f)
+	}
+	mutex.Unlock()
+	ret := Load(cookie, strings.Join(mf, ":"))
+	if ret != 0 {
+		return "application/octet-stream"
+	}
+	r := File(cookie, path)
+	return r
 }
 
 /* Get mimetype from a buffer. */
 func MimeFromBytes(b []byte) string {
-    cookie := Open(MAGIC_ERROR | MAGIC_MIME_TYPE)
-    defer Close(cookie)
-    mutex.Lock()
-    var mf []string
-    for f := range magicFiles {
-        mf = append(mf, f)
-    }
-    mutex.Unlock()
-    ret := Load(cookie, strings.Join(mf, ":"))
-    if ret != 0 {
-        return "application/octet-stream"
-    }
-    r := Buffer(cookie, b)
-    return r
+	cookie := Open(MAGIC_ERROR | MAGIC_MIME_TYPE)
+	defer Close(cookie)
+	mutex.Lock()
+	var mf []string
+	for f := range magicFiles {
+		mf = append(mf, f)
+	}
+	mutex.Unlock()
+	ret := Load(cookie, strings.Join(mf, ":"))
+	if ret != 0 {
+		return "application/octet-stream"
+	}
+	r := Buffer(cookie, b)
+	return r
 }
